04da1b002f7eb7c82948bdf0dbe280500e242bfc
Albredka <cf19albert.campos@iesjoandaustria.org>
2020-09-23 18:06:45 +0200

    dual

---
+++ dual/ejercicios_java/gat_renat/GatPersa.java
@@ -0,0 +1,36 @@
+public class GatPersa {
+    int vides = 7;   // vides disponibles del gat Persa
+    public static void main(String[] args) {
+        GatPersa renat;           // declarem la referència al gat
+        renat = new GatPersa();   // creem la instància del gat Persa.
+        System.out.println("Al gat Renat li queden " + renat.vides + " vides");
+    }
+}
+
+// Quin ha de ser el nom del fitxer que contingui aquest codi perquè funcioni?
+
+/* GatPersa.java */
+
+
+// A quina línia està declarada la variable renat que apareix a la línia 6?
+
+/* A la linea 5 */
+
+
+// A quina línia s'assigna el valor que es mostra per pantalla?
+
+/* A la linea 3 */
+
+
+//  Quin valor apareix si a aquesta línia no s'assigna cap valor?
+
+/* Null */
+
+
+//  Què passaria sí la línia 5 no hi fos? Perquè creus?
+
+
+//  Perquè creus que el comentari de la línia 4 parla de referència?
+
+
+//  La línia 5 parla d'instància. En aquest context, una instància es pot entendre com el contingut real al que fa referència la variable renat. Es diu que la instància es construeix amb l'operador new i això implica bàsicament que es reserva espai de memòria per a allotjar tota la informació relativa a la instància.
