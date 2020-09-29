0833771734cabbfe7d773a926cb713815c3c6e49
Albredka <cf19albert.campos@iesjoandaustria.org>
2020-09-29 17:27:32 +0200

    generador de turnos

---
+++ dual/ejercicios_java/prueba2/Torn.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Torn {
+    
+    protected int numero;
+    protected String torn;
+    ArrayList <Torn> cua = new ArrayList<Torn>();
+
+    // Getters
+
+    public int getNumero() {
+        return numero;
+
+    }
+
+    public String getTorn() {
+        return torn;
+
+    }
+
+    // Setters
+
+    public void setNumero(final int nouNumero) {
+        this.numero = nouNumero;
+    }
+
+    public void setTorn(final String nouTorn) {
+        this.torn = nouTorn;
+    }
+
+    // Methods
+
+    public void generaTorn(){
+        
+    }
+
+    public void assignarTorn(){
+
+    }
+}  
+
--- dual/ejercicios_java/prueba2/Verduleria.java
+++ dual/ejercicios_java/prueba2/Verduleria.java
@@ -1,102 +1,12 @@
 import java.util.ArrayList;
+//import Client;
+//import Torn;
 
 public class Verduleria {
-
-    public class Client{
-        protected String nom;
-        protected String DNI;
-        protected String direccio;
-        protected int telefon;
-
-        ArrayList <Client> clients = new ArrayList<Client>();
-
-        public void Client(){
-            clients.add(new Client());
-        }
-
-        @Override
-        public String toString(){
-            return "[dni = " + DNI + ", nom = " + nom + ", direccio = " + direccio + "telefon = " + String.valueOf(telefon) + "]";
-        }
-
-        // Getters
-
-        public String getNom(){
-            return nom;
-        }
-
-        public String getDNI(){
-            return DNI;
-        }
-
-        public String getDireccio(){
-            return direccio;
-        }
-
-        public int getTelefon(){
-            return telefon;
-        }
-
-        // Setters
-
-        public void setNom(final String nouNom) {
-            this.nom = nouNom;
-
-        }
-
-        public void setDNI(final String nouDNI) {
-            this.DNI = nouDNI;
-
-        }
-
-        public void setDireccio(final String nouDireccio) {
-            this.direccio = nouDireccio;
-        }
-
-        public void setTelefon(final int nouTelefon) {
-            this.telefon = nouTelefon;
-
-        }
-
-    }
-
-    public class Torn {
-        protected int numero;
-        protected String torn;
-        ArrayList <Torn> cua = new ArrayList<Torn>();
-
-        public void Torn(final Client cli) {
-
-            Object client;
-
-            client = cli;
-
-        }
-
-        // Getters
-
-        public int getNumero() {
-            return numero;
-
-        }
-
-        public String getTorn() {
-            return torn;
-
-        }
-
-        // Setters
-
-        public void setNumero(final int nouNumero) {
-            this.numero = nouNumero;
-        }
-
-        public void setTorn(final String nouTorn) {
-            this.torn = nouTorn;
-        }
-    }
-
     public static void main(final String[] args) {
+        Client client1 = new Client("Laia", "41077789T", "Laselles 131", "607928492");
+        
+        System.out.println(client1);
 
-    }
+    } 
 }
