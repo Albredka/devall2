d9d7ffa8404fed87a99a407d1a7b5edd15ae3f3d
Albredka <cf19albert.campos@iesjoandaustria.org>
2020-10-20 14:13:51 +0200

    EJERCICIOS 1

---
+++ m6/javascript_ejercicios/dist/ejercicio9.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* Crear una aplicació que permeti realitzar la conversió entre temperatures de  ºC a ºF  i de ºF a ºC, d’acord amb la següent fórmula: 
+            ºF = (9.0/5.0)* (ºC)+32.  */
+function CaF(c) {
+  var f = 0;
+  f = 9 / 5 * c + 32;
+  return f;
+}
---
+++ m6/javascript_ejercicios/ejercicio8.js
@@ -0,0 +1,20 @@
+
+/* Demanar un número a l’usuari que indicarà un nombre de dies. Mostrar per pantalla la conversió d’aquest número a anys, mesos i dies (no cal tenir presents els anys de traspàs). Podeu suposar que tots els mesos tenen 30 dies. */
+
+function anysMesosDies(diesX){
+    
+
+    var anys = Math.floor(diesX / 365);
+    diesX = (diesX % 365);
+
+    var mesos = Math.floor(diesX / 30);
+    dies = (diesX % 30);
+
+    console.log(anys + " - " + mesos + " - " + dies);
+}
+
+var diesX = parseInt(prompt());
+
+anysMesosDies(diesX);
+
+
